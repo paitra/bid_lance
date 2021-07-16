9496515ee58db8f23b5255d11693a6984daa8b3e
paitra <paitra@users.noreply.github.com>
2021-07-15 21:22:27 -0300

    inserção scrollDirection

--- lib/pages/lance.dart
+++ lib/pages/lance.dart
@@ -1,4 +1,3 @@
-
 import 'package:bid_leilao/service/Api.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -9,50 +8,117 @@ class Lance extends StatefulWidget {
 }
 
 class _Lance extends State<Lance> {
+  @override
   Widget build(BuildContext context) {
-        return   Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.arrow_drop_down_circle),
-                  title: const Text('Card title 1'),
-                  subtitle: Text(
-                    'Secondary Text',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
-                  children: [
-                    FlatButton(
-                      textColor: const Color(0xFF6200EE),
-                      onPressed: () {
-                        // Perform some action
-                      },
-                      child: const Text('ACTION 1'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 400,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Container(
+                          height: 120.0,
+                          width: 400.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/teste.png'),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: BoxShape.rectangle,
+                          ),
+                        ),
+                        Container(
+                          width: 200,
+                          color: Colors.blueGrey,
+                        ),
+                        Container(
+                          width: 200,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          width: 200,
+                          color: Colors.grey,
+                        )
+                      ],
                     ),
-                    FlatButton(
-                      textColor: const Color(0xFF6200EE),
-                      onPressed: () {
-                        // Perform some action
-                      },
-                      child: const Text('ACTION 2'),
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                     
+                      scrollDirection: Axis.vertical,
+                      children: <Widget>[
+
+                        Container(
+                          width: 50,
+                          height: 40,
+                          color: Colors.yellowAccent,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.yellowAccent,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 100,
+                          color: Colors.red,
+                        )
+                      ],
                     ),
-                  ],
-                ),
-                Image.asset('assets/teste.png'),
-                Image.asset('assets/logo.png'),
-              ],
+                  ),
+                  Container(
+                    height: 200,
+                    child: GridView.count(
+                      scrollDirection: Axis.horizontal,
+                      crossAxisCount: 2,
+                      children: List.generate(50, (index) {
+                        return Container(
+                          child: Card(
+                            color: Colors.amber,
+                          ),
+                        );
+                      }),
+                    ),
+                  )
+                ],
+              ),
             ),
-          );
-  
+          ),
+        ),
+      ),
+    );
   }
 }
